dba2157d59ea2f423c24c67ec3965abbff65b4f7
Sun Daowen <sundaowen@gmail.com>
2017-02-23 10:25:53 +0800

    fix pause

--- sdw_shell.cpp
+++ sdw_shell.cpp
@@ -7,6 +7,6 @@ void Pause()
 #if SDW_COMPILER == SDW_COMPILER_MSC
 	system("PAUSE");
 #else
-	system("read -rsp \"Press any key to continue . . . \" -n 1");
+	execlp("bash", "bash", "-c", "read -rsp \"Press any key to continue . . . \" -n 1 && echo", nullptr);
 #endif
 }
