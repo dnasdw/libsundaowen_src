f4ad71c30ce3343f6d37170efb42cd0844981bec
Sun Daowen <sundaowen@gmail.com>
2017-04-06 02:15:41 +0800

    add crc32

---
+++ sdw_crc32.h
@@ -0,0 +1,8 @@
+#ifndef LIBSUNDAOWEN_SDW_CRC32_H_
+#define LIBSUNDAOWEN_SDW_CRC32_H_
+
+#include "sdw_type.h"
+
+u32 UpdateCRC32(u8* a_pData, u32 a_uSize, u32 a_uPreviousCRC32 = 0);
+
+#endif	// LIBSUNDAOWEN_SDW_CRC32_H_
