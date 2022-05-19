eef966b88d180d05d4e7ae26819348bef2015496
Sun Daowen <sundaowen@gmail.com>
2022-05-19 21:19:30 +0800

    add UChdir and UGetcwd

--- sdw_dir.h
+++ sdw_dir.h
@@ -5,9 +5,13 @@
 #include "sdw_type.h"
 
 #if SDW_PLATFORM == SDW_PLATFORM_WINDOWS
+#define UChdir _wchdir
+#define UGetcwd _wgetcwd
 #define Mkdir _mkdir
 #define UMkdir _wmkdir
 #else
+#define UChdir chdir
+#define UGetcwd getcwd
 #define Mkdir(x) mkdir((x), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)
 #define UMkdir Mkdir
 #endif
